788434be88a9d9e6dc43eb680dd48859f41b5e8b
Taras Kulyk <taras.kulyk1@gmail.com>
2021-07-02 00:04:23 +0300

    Check vue 3 works

--- resources/js/Views/Index.vue
+++ resources/js/Views/Index.vue
@@ -1,9 +1,19 @@
 <template>
   <h1>Index</h1>
+  <h2>{{ value }}</h2>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+export default {
+  setup() {
+    const value = ref("some value");
+
+    return {
+      value,
+    };
+  },
+};
 </script>
 
 <style>
