09a6a605eeb23d7b680fb84669860334083e88e3
Taras Kulyk <taras.kulyk1@gmail.com>
2021-09-20 23:55:19 +0300

    Add unit test for create user

---
+++ tests/Unit/App/Services/UserService/CreateTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit\App\Services\AuthService;
+
+use Tests\Unit\TestCase;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Services\UserService;
+use App\Models\User;
+
+class CreateTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function success()
+    {
+        /** @var UserService */
+        $service = $this->partialMock(UserService::class);
+
+        $result = $service->create([
+            'name' => 'some name',
+            'email' => 'some email',
+            'password' => 'secret',
+        ]);
+
+        $this->assertInstanceOf(User::class, $result);
+        $this->assertTrue(Hash::check('secret', $result->password));
+    }
+}
